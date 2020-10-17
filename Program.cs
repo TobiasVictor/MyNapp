0a3e2d7d6f317be22b95f78919d8be9654e58121
Tobias Victor <bboyvictor17@yahoo.com>
2020-10-17 09:19:49 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyNapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
